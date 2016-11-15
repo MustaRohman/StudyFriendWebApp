7b67051f6248434996f55e991718ca154f416e03
Mustafizur Rohman <mustarohman@gmail.com>
2016-11-15 12:25:12 +0000

    Added public folder which will hold web pages. Added eslinter

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-	res.send('Welcome to the Post-Trump Internet!')
-})
+app.use(express.static('public'));
 
-app.listen(3000, function () {
-	console.log("Connected!")
-})
+app.get('/', function(req, res) {
+  res.send('Welcome to the Post-Trump Internet!');
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Connected!');
+});
