c8cc7ec82d9052f64b4bcf07fa055dbbe0ab6c28
Mustafizur Rohman <mustarohman@gmail.com>
2016-11-29 20:42:28 +0000

    babel-setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static('public'));
 
+const name = 'Musta';
+
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on Port 3000');
+  console.log(`Hello ${name}, Listening on Port 3000`);
 });
