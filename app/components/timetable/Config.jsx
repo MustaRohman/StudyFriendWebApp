289cb10ab5fff0c6c7a23e71650fea03585bde35
Mustafizur Rohman <mustarohman@gmail.com>
2017-02-12 20:29:53 +0000

    Now Config now handles state changes

--- app/components/timetable/Config.jsx
+++ app/components/timetable/Config.jsx
@@ -18,13 +18,21 @@ const Config = React.createClass({
       reward: this.props.reward
     };
   },
+  handleChange: function() {
+    this.setState({
+      name: this.refs.timetableName.value,
+      session: this.refs.session.value,
+      break: this.refs.break.value,
+      reward: this.refs.reward.value
+    });
+  },
   render: function() {
     return (
       <div>
         <h2>Configuration</h2>
         <label>
           Timetable Name:
-          <input type="text" name="timetableName" value={this.state.name}/>
+          <input type="text"  value={this.state.name} ref="timetableName" onChange={this.handleChange}/>
         </label>
         <h3>Dates</h3>
         <label>
@@ -38,15 +46,18 @@ const Config = React.createClass({
         <h3>Durations</h3>
         <label>
           Session:
-          <input type="range" min="30" max="60" value={this.state.session}/>
+          <input ref="session" type="range" min="30" max="60" step="5" value={this.state.session} onChange={this.handleChange}/>
+          <input type="textarea" value={this.state.session}/>
         </label>
         <label>
           Break:
-          <input type="range" min="10" max="30" value={this.state.break}/>
+          <input ref="break" type="range" min="10" max="30" step="5" value={this.state.break} onChange={this.handleChange}/>
+          <input type="textarea" value={this.state.break}/>
         </label>
         <label>
           Reward:
-          <input type="range" min="30" max="90" value={this.state.reward}/>
+          <input ref="reward" type="range" min="30" max="90" step="5" value={this.state.reward} onChange={this.handleChange}/>
+          <input type="textarea" value={this.state.reward}/>
         </label>
       </div>
     );
