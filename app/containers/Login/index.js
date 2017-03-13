d8138fe1ce08ff7a6620422aa0f73c3c6539e40f
Mustafizur Rohman <mustarohman@gmail.com>
2017-03-13 21:13:17 +0000

    login styling

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import { GoogleLogin } from 'react-google-login-component';
-// 375688671713-nlf5vnm3i77latudv441or2nfiu0n9ok.apps.googleusercontent.com
+import Paper from 'material-ui/Paper';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import styles from './style.css';
 
 export default class  Login extends Component {
   constructor(props, context) {
@@ -15,15 +18,20 @@ export default class  Login extends Component {
 
   render() {
     return (
-      <div>
-        <h1>StudyFriend</h1>
-        <h2>Welcome to the dynamic revision planner!</h2>
-        <GoogleLogin socialId="375688671713-nlf5vnm3i77latudv441or2nfiu0n9ok.apps.googleusercontent.com"
-                     class="google-login"
-                     scope="profile"
-                     responseHandler={this.responseGoogle}
-                     buttonText="Login With Google" />
-      </div>
+      <MuiThemeProvider>
+          <Paper className={'login'} zDepth={2}>
+            <div>
+              <h1>StudyFriend</h1>
+              <h2>Welcome to the dynamic revision planner!</h2>
+              <GoogleLogin socialId="375688671713-nlf5vnm3i77latudv441or2nfiu0n9ok.apps.googleusercontent.com"
+                           class="google-login"
+                           scope="profile"
+                           responseHandler={this.responseGoogle}
+                           buttonText="Login With Google"
+                           className={'google-class'}/>
+               </div>
+           </Paper>
+      </MuiThemeProvider>
     );
   }
 }
